6289004ecd32d27ddcab949e500ff8e9781880e4
tomigorn <tomigorn@gmail.com>
2025-05-19 15:22:39 +0200

    AnimalFactoryExample added

--- InterviewExamples/Examples/AnimalFactoryExample.cs
+++ InterviewExamples/Examples/AnimalFactoryExample.cs
@@ -8,5 +8,64 @@ namespace InterviewExamples.Examples
 {
     internal class AnimalFactoryExample
     {
+        public static void Run() //in a standalone app this would be static void Main(string[] args)
+        {
+            var factory = new AnimalFactory();
+
+            var a1 = factory.GetAnimal("cat");
+            Console.WriteLine(a1.Speak());
+
+            var a2 = factory.GetAnimal("dog");
+            Console.WriteLine(a2.Speak());
+
+            var a3 = factory.GetAnimal("bird");
+            Console.WriteLine(a3.Speak());
+        }
+    }
+
+    public interface IAnimal
+    {
+        string Speak();
+    }
+
+    public class Cat : IAnimal
+    {
+        public string Speak() => "Meow";
+    }
+
+    public class Dog : IAnimal
+    {
+        public string Speak() => "Woof";
+    }
+
+    public class AnimalFactory
+    {
+        private readonly Dictionary<string, Func<IAnimal>> _creators = new()
+    {
+        { "cat", () => new Cat() },
+        { "dog", () => new Dog() }
+    };
+
+        public IAnimal GetAnimal(string type)
+        {
+            if (_creators.TryGetValue(type, out var creator))
+            {
+                return creator();
+            }
+
+            return new UnknownAnimal(type);
+        }
+    }
+
+    public class UnknownAnimal : IAnimal
+    {
+        private readonly string _type;
+
+        public UnknownAnimal(string type)
+        {
+            _type = type;
+        }
+
+        public string Speak() => $"Unknown animal: {_type}";
     }
 }
--- InterviewExamples/Program.cs
+++ InterviewExamples/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InterviewExamples.Examples;
+using System;
+
+namespace InterviewExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an example to run:");
+            Console.WriteLine("1 - Animal Factory");
+
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    AnimalFactoryExample.Run();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
