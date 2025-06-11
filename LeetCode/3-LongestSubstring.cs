d066f09bb3d50899d54601a98e22b8fbff2d3eb7
tomigorn <tomigorn@gmail.com>
2025-06-12 01:41:04 +0200

    LeetCode "Longest Substring" O(n) solution

--- LeetCode/3-LongestSubstring.cs
+++ LeetCode/3-LongestSubstring.cs
@@ -2,6 +2,41 @@ using System.Linq;
 
 public class LeetCode_LongestSubstring
 {
+
+    /// <summary>
+    /// Solution with Time Complexity of O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> set = new();
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+
+        while (right < s.Length)
+        {
+            if (!set.Contains(s[right]))
+            {
+                set.Add(s[right]);
+                maxLength = Math.Max(maxLength, right - left + 1);
+                right++;
+            }
+            else
+            {
+                set.Remove(s[left]);
+                left++;
+            }
+        }
+        return maxLength;
+    }
+
+    /// <summary>
+    /// Suboptimal Solution with complexity of O(n^2)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
     public int LengthOfLongestSubstringBadSolution(string s)
     {
         int longestSubstringLength = 0;
